1f6e0c7b3d9acb25e22e976b13b1f187e4d828fa
rebeccaquey <rebecca_quey@yahoo.com>
2020-05-26 22:39:04 -0700

    implement videoList features

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,8 @@
+import VideoList from './VideoList.js';
+import exampleVideoData from '../data/exampleVideoData.js';
+// import VideoPlayer from './VideoPlayer.js';
+// import Search from './Search.js';
+
 var App = () => (
   <div>
     <nav className="navbar">
@@ -10,7 +15,7 @@ var App = () => (
         <div><h5><em>videoPlayer</em> view goes here</h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+          <VideoList videos={exampleVideoData} />
       </div>
     </div>
   </div>
