07c79090a281c578b5c92e1843f84b4d86264b2d
rebeccaquey <rebecca_quey@yahoo.com>
2020-05-27 00:10:21 -0700

    pass specs for videoListEntry and VideoPlayer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,60 @@
 import VideoList from './VideoList.js';
 import exampleVideoData from '../data/exampleVideoData.js';
-// import VideoPlayer from './VideoPlayer.js';
+import VideoPlayer from './VideoPlayer.js';
 // import Search from './Search.js';
 
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
-      </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <VideoList videos={exampleVideoData} />
+// var App = () => (
+//   <div>
+//     <nav className="navbar">
+//       <div className="col-md-6 offset-md-3">
+//         <div><h5><em>search</em> view goes here</h5></div>
+//       </div>
+//     </nav>
+//     <div className="row">
+//       <div className="col-md-7">
+//         {/* do we hard code this data input? does it only take in the first video? */}
+//         <VideoPlayer video={exampleVideoData[0]} />
+//       </div>
+//       <div className="col-md-5">
+//         <VideoList videos={exampleVideoData} />
+//       </div>
+//     </div>
+//   </div>
+// );
+
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      //needs to keep track of all the videos in the video list and the current video in the player
+    }
+  }
+  render() {
+    return (
+    <div>
+      <nav className="navbar">
+        <div className="col-md-6 offset-md-3">
+          <div><h5><em>search</em> view goes here</h5></div>
+        </div>
+      </nav>
+      <div className="row">
+        <div className="col-md-7">
+          {/* do we hard code this data input? does it only take in the first video? */}
+          <VideoPlayer video={exampleVideoData[0]} />
+        </div>
+        <div className="col-md-5">
+          <VideoList videos={exampleVideoData} />
+        </div>
       </div>
     </div>
-  </div>
-);
+    )
+  }
+}
+
+
+
+
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,11 +1,20 @@
-var VideoPlayer = () => (
+var VideoPlayer = (props) => (
   <div className="video-player">
+    {/* {console.log('props here', props)} */}
     <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" allowFullScreen></iframe>
+      <iframe
+        className="embed-responsive-item"
+        //using string concatenation:
+        src={'https://www.youtube.com/embed/' + props.video.id.videoId}
+        // using template literals:
+        // src=`https://www.youtube.com/embed/${props.video.id.videoId}`
+        allowFullScreen
+      >
+      </iframe>
     </div>
     <div className="video-player-details">
-      <h3>Video Title</h3>
-      <div>Video Description</div>
+      <h3>{props.video.snippet.title}</h3>
+      <div>{props.video.snippet.description}</div>
     </div>
   </div>
 );
